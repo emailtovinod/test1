07ba88f27810cf9265ed0e2a36e5f0d92ba476a5
Vino Alex <valex@redhat.com>
2019-07-14 18:19:20 +0530

    test4

--- resource.php
+++ resource.php
@@ -5,15 +5,15 @@
 
 <?php
   $my_pod_name = getenv('my_pod_name');
-  echo nl2br("<b><font color=blue>My Name: $my_pod_name</font></b> \r\n");
+  echo nl2br("<b><font color=blue><center>My Name: $my_pod_name</font></center></b> \r\n");
   $my_cpu_request = getenv('my_cpu_request');
-  echo nl2br ("<b><font color=green>My CPU Request: $my_cpu_request</font></b>  \r\n");
+  echo nl2br ("<b><font color=green><center>My CPU Request: $my_cpu_request</font></center></b>  \r\n");
   $my_memory_request = getenv('my_memory_request');
-  echo nl2br ("<b><font color=green>My Memory Request: $my_memory_request</font></b>  \r\n");
+  echo nl2br ("<b><font color=green><center>My Memory Request: $my_memory_request</font></center></b>  \r\n");
   $my_cpu_limit = getenv('my_cpu_limit');
-  echo nl2br ("<b><font color=red>My CPU Limit: $my_cpu_limit</font></b>  \r\n");
+  echo nl2br ("<b><font color=red><center>My CPU Limit: $my_cpu_limit</font></center></b>  \r\n");
   $my_memory_limit = getenv('my_memory_limit');
-  echo nl2br ("<b><font color=red>My Memory Limit: $my_memory_limit</font></b>  \r\n")
+  echo nl2br ("<b><font color=red><center>My Memory Limit: $my_memory_limit</font></center></b>  \r\n")
 ?> 
 </body>
 </html>  
