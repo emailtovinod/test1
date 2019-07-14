1ba8d32bf4b9f4e6fe88f79e42a13eae9fc63c85
Vino Alex <valex@redhat.com>
2019-07-14 13:09:16 +0530

    test2

--- php-test.php
+++
@@ -1,4 +0,0 @@
-<?php
-  $db = getenv('DB_NAME'); // Gets the database name
-  echo "Database name: $db  \r\n"
-?>   
---
+++ resource.php
@@ -0,0 +1,8 @@
+<?php
+  $db = getenv('DB_NAME'); // Gets the database name
+  echo "My Name: $my_pod_name  \r\n"
+  echo "My CPU Request: $my_cpu_request  \r\n"
+  echo "My Memory Request: $my_memory_request  \r\n"
+  echo "My CPU Limit: $my_cpu_limit  \r\n"
+  echo "My Memory Limit: $my_memory_limit  \r\n"
+?>   
